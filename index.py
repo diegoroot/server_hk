f89f2c2b0bd6e2fa10f1d15907fa387d3b11dbc8
diegoroot <jabg58@hotmail.com>
2020-06-06 01:15:38 -0500

    json

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def consul():
-	return "hola"
+	return "{'1': {'semestre': 15, 'correo': 'royer123@gmail.com', 'celular': 123, 'nombre': 'Royer', 'password': 'hola', 'id': 1234}, '0': {'semestre': 8, 'correo': 'luchocorrealviveros@gmail.com', 'celular': 3148527176, 'nombre': 'Luis', 'password': 'hola', 'id': 1116282977}, '2': {'semestre': 8, 'correo': 'gian123@gmail.com', 'celular': 123, 'nombre': 'gian', 'password': 'hola', 'id': 123}}"
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
